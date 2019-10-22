a71ae63b1dc451e459a7a050e6680f797a274693
RocioE <48306304+RocioE@users.noreply.github.com>
2019-10-21 21:18:15 -0700

    added routers

--- mongo.js
+++ mongo.js
@@ -7,3 +7,10 @@ function article(title, content, author){
     this.content= content;
     this.author= author;
 }
+
+var article1 = new article("title", "content", "author");
+
+db.article.save(article1, function(err, savedArticle){
+    if( err savedArticle) console.log("article " + err);
+    else console.log("Article + savedArticle + saved");
+});
